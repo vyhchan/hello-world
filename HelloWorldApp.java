cf8045d66dc17fc41ae4d0a8efcfcce911f71454
vyhchan <chamyandvictor@gmail.com>
2017-12-28 00:01:24 +1100

    recompiledapplication

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,5 +2,6 @@ class HelloWorldApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Universe!");
+		System.out.println("Welcome Home!");
 	}
 }
